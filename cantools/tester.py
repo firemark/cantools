4ab22f7bdea228fd3bf787449c038cc29267e0da
Firemark <marpiechula@gmail.com>
2023-01-31 21:03:02 +0100

    Fix encoding decimal values in the Tester class

--- cantools/tester.py
+++ cantools/tester.py
@@ -271,7 +271,9 @@ class Message(UserDict, object):
             maximum = 0 if not signal.maximum else signal.maximum
             if signal.initial:
                 # use initial signal value (if set)
-                initial_sig_values[signal.name] = (signal.initial * signal.decimal.scale) + signal.decimal.offset
+                value = (signal.initial * signal.decimal.scale) + signal.decimal.offset
+                transform = float if signal.is_float else round
+                initial_sig_values[signal.name] = transform(value)
             elif signal.is_multiplexer:
                 initial_sig_values[signal.name] = mplex_settings.get(signal.name, 0)
             elif minimum <= 0 <= maximum:
